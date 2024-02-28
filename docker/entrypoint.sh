d5f32f9f639b6cf03a7040daee951e5d4da3104e
rwbot <rwbotx@gmail.com>
2024-02-28 03:29:15 -0700

    functional

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,19 +6,18 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 echo "Sourced ROS ${ROS_DISTRO}"
 
 # Source the base workspace, if built
-if [ -f /turtlebot3_ws/install/setup.bash ]
+if [ -f /base_ws/devel/setup.bash ]
 then
-  source /turtlebot3_ws/install/setup.bash
-  export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix turtlebot3_gazebo)/share/turtlebot3_gazebo/models
-  echo "Sourced TurtleBot3 base workspace"
+  echo "Tring to source base_ws"
+  source /base_ws/devel/setup.bash
+  echo "Sourced base_ws"
 fi
 
 # Source the overlay workspace, if built
-if [ -f /overlay_ws/install/setup.bash ]
+if [ -f /overlay_ws/devel/setup.bash ]
 then
-  source /overlay_ws/install/setup.bash
-  export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix tb3_worlds)/share/tb3_worlds/models
-  echo "Sourced autonomy overlay workspace"
+  source /overlay_ws/devel/setup.bash
+  echo "Sourced overlay_ws"
 fi
 
 # Execute the command passed into this entrypoint
